46787af7dcfe4b239491f306950c780425bd67ad
diegomestre2 <diegogomest@gmail.com>
2018-06-17 11:35:21 +0200

    Adding kernel to multiply matrix using shared memory

--- src/matrixMul_shared.cu
+++ src/matrixMul_shared.cu
@@ -47,7 +47,100 @@ __device__ Matrix getSubMatrix(Matrix A, int row, int column){
 	return Asub;
 }
 
+/*
+Matrix Multiplication Device kernel
+*/
+__global__
 void matrixMul(const Matrix A, const Matrix B, Matrix C){
 
+	int blockRow = blockIdx.y;
+	int blockCol = blockIdx.x;
 
+	Matrix Csub = getSubMatrix(C, blockRow, blockCol);
+
+	float Cvalue = 0;
+	int row = threadIdx.y;
+	int col = threadIdx.x;
+
+	for(int m = 0; m < (A.width / BLOCK_SIZE); ++m){
+
+		Matrix Asub = getSubMatrix(A, blockRow, m);
+		Matrix Bsub = getSubMatrix(B, m, blockCol);
+
+		__shared__ float As[BLOCK_SIZE][BLOCK_SIZE];
+		__shared__ float Bs[BLOCK_SIZE][BLOCK_SIZE];
+
+		As[row][col] = getElement(Asub, row, col);
+		Bs[row][col] = getElement(Bsub, row, col);
+
+		_syncthreads();
+
+		for(int e = 0; e < BLOCK_SIZE; ++e){
+			Cvalue += As[row][e] * Bs[e][col];
+		}
+		_syncthreads(); 
+	}
+	setElement(Csub, row, col, Cvalue);
 }
+
+/*
+Matrix Multiplication Host
+*/
+void matrixMultiplicaiton(Matrix A, Matrix B, Matrix C){
+
+	Matrix d_A, d_B, d_C;
+
+	d_A.width = d_A.stride = A.width;
+	d_B.width = d_B.stride = B.width;
+	d_C.width = d_C.stride = C.width;
+	
+	d_A.height = A.height;
+	d_B.height = B.height;
+	d_C.height = C.height;
+
+	size_t size_A = A.width * A.height * sizeof(float);
+	size_t size_B = B.width * B.height * sizeof(float);
+	size_t size_C = C.width * C.height * sizeof(float);
+
+	cudaMalloc(&d_A.elements, size_A);
+	cudaMalloc(&d_B.elements, size_B);
+	cudaMalloc(&d_C.elements, size_C);
+
+	cudaMemcpy(d_A.elements, A.elements, size_A, cudaMemcpyHostToDevice);
+	cudaMemcpy(d_B.elements, B.elements, size_B, cudaMemcpyHostToDevice);
+
+	dim3 dimBLock(BLOCK_SIZE, BLOCK_SIZE);
+	dim3 dimGrid(B.width / dimBLock.x, A.height / dimBLock.y);
+
+	matrixMul<<<dimGrid, dimBLock>>>(d_A, d_B, d_C);
+
+	cudaMemcpy(C.elements, d_C.elements, size_C, cudaMemcpyDeviceToHost);
+
+	cudaFree(d_A.elements);
+	cudaFree(d_B.elements);
+	cudaFree(d_C.elements);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
